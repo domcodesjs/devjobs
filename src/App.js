3f70771191bd3890ccdb64c7c887216a12c0715f
Donovan <donovanle18@gmail.com>
2020-10-27 14:00:19 -0500

    fetch requests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useFetchJobs from "./useFetchJobs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { jobs, loading, error } = useFetchJobs();
+
+    return (
+        <div>
+            {loading && <h1>Loading...</h1>}
+            {error && <h1>Error....</h1>}
+            <h1>{jobs.length}</h1>
+        </div>
+    );
 }
 
 export default App;
